1e3d63a5c7abdc99d5aa114bef14a1d4e40a938d
Willis <willismt@8c85907f7d4a.ant.amazon.com>
2021-03-02 14:26:13 -0500

    adding remove method

--- src/main/java/com/shipmentEvents/demo/Map.java
+++ src/main/java/com/shipmentEvents/demo/Map.java
@@ -6,11 +6,18 @@ import java.util.concurrent.ConcurrentMap;
 public class Map {
     private static ConcurrentMap<String,String> map = new ConcurrentHashMap<>();
 
-    public String getValue(String key) {
+    public static void main(String[] args) {
+
+    }
+    public static String checkExists(String key) {
         if(map.containsKey(key)) {
             return map.get(key);
         }
 
         return null;
+    }
+
+    public static void removeItem(String key) {
+        map.remove(key);
     }
 }
