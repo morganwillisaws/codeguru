cb713ee727a1386a575eb4ca531f52e07a386757
Willis <willismt@8c85907f7d4a.ant.amazon.com>
2021-03-02 13:48:09 -0500

    adding non-synchronized counter that could have race conditions

---
+++ src/main/java/com/shipmentEvents/demo/Counter.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Counter {
+        private int counter = 0;
+
+        public void increment() {
+            counter++;
+        }
+
+        public int getValue() {
+            return counter;
+        }
+}
