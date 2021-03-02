82ad8a0e14f6c74e38365f0394f2f5e27d98a654
Willis <willismt@8c85907f7d4a.ant.amazon.com>
2021-03-02 12:34:50 -0500

    adding test file using Scanner

---
+++ src/main/java/com/shipmentEvents/demo/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Please enter your name");
+        String name = in.nextLine();
+
+        System.out.println("Please select Rock, Paper, or Scissors");
+
+    }
+}
