c1c1c76acc1f0e0f1082cb15b228875c145087ef
Willis <willismt@8c85907f7d4a.ant.amazon.com>
2021-03-02 13:29:13 -0500

    testing common coding problems

--- src/main/java/com/shipmentEvents/demo/Main.java
+++ src/main/java/com/shipmentEvents/demo/Main.java
@@ -8,10 +8,18 @@ public class Main {
         Scanner in = new Scanner(System.in);
 
 
-
         //adding a line here testing things
         System.out.println("Please enter your name");
         String name = in.nextLine();
+
+        do{
+            //this could be an infinite loop!
+            //String comparison using ==
+            if(name == "Morgan") {
+                break;
+            }
+
+        } while(true);
 
         System.out.println("Please select Rock, Paper, or Scissors");
 
