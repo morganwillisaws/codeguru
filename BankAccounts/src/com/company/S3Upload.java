dcc9a24fa1bd2cc08e409c78377ea2b430dc61e6
Willis <willismt@8c85907f7d4a.ant.amazon.com>
2021-03-30 12:15:14 -0400

    adding s3 file writer example

---
+++ BankAccounts/src/com/company/S3Upload.java
@@ -0,0 +1,43 @@
+package com.mycompany.app.model;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+/**
+ * Upload a file to an Amazon S3 bucket.
+ *
+ * This code expects that you have AWS credentials set up per:
+ * http://docs.aws.amazon.com/java-sdk/latest/developer-guide/setup-credentials.html
+ */
+public class S3Upload {
+    public static void main(String[] args) {
+        final String USAGE = "\n" +
+                "To run this example, supply the name of an S3 bucket and a file to\n" +
+                "upload to it.\n" +
+                "\n" +
+                "Ex: PutObject <bucketname> <filename>\n";
+
+        if (args.length < 2) {
+            System.out.println(USAGE);
+            System.exit(1);
+        }
+
+        String bucket_name = args[0];
+        String file_path = args[1];
+        String key_name = Paths.get(file_path).getFileName().toString();
+
+        System.out.format("Uploading %s to S3 bucket %s...\n", file_path, bucket_name);
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
+        try {
+            s3.putObject("aws_s3_bucket", "file_name", new File("example.txt"));
+        } catch (AmazonServiceException e) {
+            System.err.println(e.getErrorMessage());
+            System.exit(1);
+        }
+        System.out.println("Done!");
+    }
+}}
