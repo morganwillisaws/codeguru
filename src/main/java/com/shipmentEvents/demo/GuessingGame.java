31ae91fa19a6e22f20b5223066676f911a29fd45
Willis <willismt@8c85907f7d4a.ant.amazon.com>
2021-04-22 12:55:19 -0400

    fixing bugs

--- src/main/java/com/shipmentEvents/demo/GuessingGame.java
+++ src/main/java/com/shipmentEvents/demo/GuessingGame.java
@@ -1,75 +1,53 @@
-package com.company;
+package com.mycompany.app;
 
 import java.util.Scanner;
 
 public class GuessingGame {
-     public static void main(String[] args) {
-            int computerNumber = (int) (Math.random()*100 + 1);
-            System.out.println("A random number has been generated...");
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter a guess between 1 and 100");
-            int userAnswer = scanner.nextInt();
 
-            // Guess 1
-            if (userAnswer <=0 || userAnswer >100) {
-                System.out.println("Invalid response");
-            }
-            else if (userAnswer == computerNumber ){
-                System.out.println("Correct! You win! See you next time!");
-                System.exit(0);
-            }
-            else if (userAnswer > computerNumber) {
-                System.out.println("Your guess is too high, guess again.");
-            }
-            else if (userAnswer < computerNumber) {
-                System.out.println("Your guess is too low, guess again.");
-            }
-            else {
-                System.out.println("Your guess is incorrect");
-            }
+    private static Scanner scanner = new Scanner(System.in);
 
-             // Guess 2
-             System.out.println("Enter a guess between 1 and 100");
-             userAnswer = scanner.nextInt();
+    public static void main(String[] args) {
 
-             if (userAnswer <=0 || userAnswer >100) {
-                 System.out.println("Invalid response");
-             }
-             else if (userAnswer == computerNumber ){
-                 System.out.println("Correct! You win! See you next time!");
-                 System.exit(0);
-             }
-             else if (userAnswer > computerNumber) {
-                 System.out.println("Your guess is too high, guess again.");
-             }
-             else if (userAnswer < computerNumber) {
-                 System.out.println("Your guess is too low, guess again.");
-             }
-             else {
-                 System.out.println("Your guess is incorrect");
-             }
+        // Generate computer number
+        int computerNumber = generateNumber();
 
-             // Guess 3
-             System.out.println("Enter a guess between 1 and 100");
-             userAnswer = scanner.nextInt();
+        // Guess 1
+        int userAnswer = -1;
+        for(int i = 0; i < 3; i++) {
+            userAnswer = promptForInput();
+            determineOutput(userAnswer, computerNumber);
+        }
 
-             if (userAnswer <=0 || userAnswer >100) {
-                 System.out.println("Invalid response");
-             }
-             else if (userAnswer == computerNumber ){
-                 System.out.println("Correct! You win! See you next time!");
-                 System.exit(0);
-             }
-             else if (userAnswer > computerNumber) {
-                 System.out.println("Your guess is too high");
-             }
-             else if (userAnswer < computerNumber) {
-                 System.out.println("Your guess is too low, guess again.");
-             }
-             else {
-                 System.out.println("Your guess is incorrect");
-             }
+        System.out.println("Sorry for your luck, restart the program to play again!");
+    }
 
-            System.out.println("Sorry for your luck, restart the program to play again!");
+    private static int generateNumber() {
+        int computerNumber = (int) (Math.random()*100 + 1);
+        System.out.println("A random number has been generated...");
+        return computerNumber;
+    }
+
+    private static int promptForInput() {
+        System.out.println("Enter a guess between 1 and 100");
+        return scanner.nextInt();
+    }
+
+    private static void determineOutput(int userAnswer, int computerNumber) {
+        if (userAnswer <=0 || userAnswer >100) {
+            System.out.println("Invalid response");
+        }
+        else if (userAnswer == computerNumber ){
+            System.out.println("Correct! You win! See you next time!");
+            System.exit(0);
+        }
+        else if (userAnswer > computerNumber) {
+            System.out.println("Your guess is too high, guess again.");
+        }
+        else if (userAnswer < computerNumber) {
+            System.out.println("Your guess is too low, guess again.");
+        }
+        else {
+            System.out.println("Your guess is incorrect");
+        }
     }
 }
